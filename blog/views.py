42c16d9672d125b50988ae60ab7a6d802323e6fe
ujinhee <ujinhee@gmail.com>
2015-10-03 16:17:37 +0900

    publish button

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
+    posts = Post.objects.all().order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -39,3 +40,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+#
+# def publish(self):
+#     self.published_date = timezone.now()
+#     self.save()
